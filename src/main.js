babac04d7ae57130a2257ee45e793171db75bc04
Knackington <keaton.haux@gmail.com>
2022-04-10 19:21:40 -0700

    Player cna control the rocket and now fire

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-console.log("hello from main.js")
+console.log("hello from main.js");
 
-let config = {
-    type: Phaser.CANVAS,
+let config = 
+{
+    type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: "menu"
+    scene: [ Menu, Play]
 }
 
-let game = new Phaser.Game(config)
+let game = new Phaser.Game(config);
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+borderUISize = game.config.height / 15;
+borderPadding = borderUISize / 3;
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,10 +1,15 @@
-class Menu extends Phaser.Scene{
-    constructor(){
-        super("menu");
+class Menu extends Phaser.Scene
+{
+    constructor()
+    {
+        super("menuScene");
     }
 
-    create(){
-        let menuText = this.add.text( 300, 240, "menu goes here");
+    create()
+    {
+        let menuText = this.add.text( 20, 20, "Rocket Patrol Menu");
         menuText.setOrigin(0.5, 0.5);
+
+        this.scene.start("playScene");
     }
 }
