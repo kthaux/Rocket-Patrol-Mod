ed6cd835f98b8ea39b3ad4b14455304245dd2dff
Knackington <keaton.haux@gmail.com>
2022-04-10 20:55:03 -0700

    Added score and ship explosions

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -18,7 +18,12 @@ class Spaceship extends Phaser.GameObjects.Sprite
         //wrap around 
         if(this.x <= 0 - this.width)
         {
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    reset()
+    {
+        this.x = game.config.width;
     }
 }
