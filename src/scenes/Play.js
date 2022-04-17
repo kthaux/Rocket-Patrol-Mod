66724abc4cbf7d34e15703a8d5ce58dfab1bf39e
Knackington <keaton.haux@gmail.com>
2022-04-16 19:41:30 -0700

    Spaceships are now bats. the rocket is now an arrow. The space background is now a cloudy night sky. explosions are now a blood splat.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,9 +11,11 @@ class Play extends Phaser.Scene
     {
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
-        this.load.image('rocket','assets/rocket.png');
-        this.load.image('spaceship', 'assets/spaceship.png');
-        this.load.image('starfield', 'assets/starfield.png');
+        this.load.spritesheet('splat', './assets/splat.png', {frameWidth: 44, frameHeight: 31, startFrame: 0, endFrame: 2});
+        this.load.image('rocket','assets/Arrow.png');
+        this.load.image('spaceship', 'assets/BatPix.png');
+        this.load.image('starfield', 'assets/CloudyNight.png');
+        this.load.image('arrow', 'assets/Arrow.png');
     }
 
     create()
@@ -24,10 +26,10 @@ class Play extends Phaser.Scene
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0x00000).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0x00000).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0x00000).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x00000).setOrigin(0, 0);
 
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         //add spaceships (x3)
@@ -44,8 +46,8 @@ class Play extends Phaser.Scene
 
         // animation config
         this.anims.create({
-            key: 'explode',
-            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+            key: 'bloodsplat',
+            frames: this.anims.generateFrameNumbers('splat', { start: 0, end: 2, first: 0}),
             frameRate: 30
         });
 
@@ -143,7 +145,7 @@ class Play extends Phaser.Scene
         ship.alpha = 0;
         // create explosion sprite at ship's position
         let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
-        boom.anims.play('explode');             // play explode animation
+        boom.anims.play('bloodsplat');             // play explode animation
         boom.on('animationcomplete', () => {    // callback after anim completes
           ship.reset();                         // reset ship position
           ship.alpha = 1;                       // make ship visible again
